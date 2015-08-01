13d6de025e8a164c62fd220ee364de5248c9d200
nitzer <nitzer@ark>
2015-08-01 20:45:12 -0300

    fix visuales y de textos

--- js/main.js
+++ js/main.js
@@ -9,8 +9,52 @@
  * Copyright 2014, Script Tutorials
  * http://www.script-tutorials.com/
  */
+function strip(html)
+{
+   var html = html.replace("<br>","||br||");  
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = html;
+   html = tmp.textContent||tmp.innerText;
+   return html.replace("||br||","<br>");  
+}
+
+function trim_words(theString, numWords) {
+    expString = theString.split(/\s+/,numWords);
+    theNewString=expString.join(" ");
+    return theNewString;
+}
 
 $(document).ready(function (){
+
+
+    jQuery.getFeed({
+
+        url: 'proxy.php?url=http://corposao.blogspot.com/feeds/posts/default',
+        success: function(feed) {
+            jQuery('#feedItems').html("");
+            var html = '';
+            
+            for(var i = 0; i < feed.items.length && i < 4; i++) {
+                
+                var item = feed.items[i];
+                
+                html += 
+                  '<div class="col-sm-6">' +
+                    '<div class="panel panel-default">' +
+                      '<div class="panel-body rssDescription">' + 
+                        '<h4>' + item.title + '</h4>' +
+                        trim_words(strip(item.description), 50) + ' ...</div>' +
+                      '<div class="panel-footer"><a class="btn btn-primary" href="'+item.link+'">Seguir Leyendo</a></div>' +
+                    '</div>' +
+                  '</div>';
+
+                console.log(item);
+            }
+            
+            jQuery('#feedItems').append(html);
+        }    
+    });
+
 
   // create a LatLng object containing the coordinate for the center of the map
   
